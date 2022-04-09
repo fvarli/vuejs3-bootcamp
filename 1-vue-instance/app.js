3df743160da20ca0460c8ea39211e45fd8d70aef
Ferzender Varli <fvarlii@gmail.com>
2022-04-10 01:03:07 +0300

    vue-instance

---
+++ 1-vue-instance/app.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            title:"Vue.js Bootcamp first day", 
+            content: "Lorem ipsum dolor sit amet..",
+            eduflow: {
+                title: "Test Title",
+                target: "_blank",
+                url: "https://ferzendervarli.com",
+                alt: "Test Alt"
+            },
+            owner: "fred",
+            coords: {
+                x: 0,
+                y: 0,
+            }
+        };
+    },
+    methods: {
+        changeTitle(title){
+            this.title = title;
+        },
+        updateCoords(message, event){
+            console.log(message, event.x, event.y)
+            this.changeTitle(`${'X: ' + event.x},${' Y: ' + event.y}`)
+            this.coords = {
+                x: 'X: ' + event.x,
+                y: 'Y: ' + event.y
+            }
+        }
+    }
+}).mount("#app")
