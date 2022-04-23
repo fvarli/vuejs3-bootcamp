81ea4ae52118b9a1a107851eeeaedfe5cafde847
Ferzender Varli <fvarlii@gmail.com>
2022-04-23 03:30:04 +0300

    vue-reactivity

---
+++ 4-reactivity-examples/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            search: "",
+            itemList: ["PHP", "CodeIgniter", "Laravel"],
+        };
+    },
+    methods: {
+        searchList() {
+            /*const filteredList = this.itemList.filter((i) => i.includes(this.search));
+            console.log("filteredList :>>", filteredList);*/
+        }
+    },
+    computed: {
+        filteredList() {
+            return this.itemList.filter((i) => i.includes(this.search));
+        },
+    },
+}).mount('#app');
