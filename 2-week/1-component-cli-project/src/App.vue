3abf9e232481d2cf640e62c1540743f021a3d98e
Ferzender Varli <fvarlii@gmail.com>
2022-05-03 12:54:31 +0300

    vue-component

---
+++ 2-week/1-component-cli-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <app-header></app-header>
+    <div class="container pt-2">
+        <h3 class="text-center">First CLI App</h3>
+    </div>
+    <CounterItem></CounterItem>
+    <CounterItem></CounterItem>
+</template>
+
+<script>
+import CounterItem from "@/components/CounterItem";
+
+export default {
+    components: {
+        CounterItem: CounterItem
+    }
+}
+</script>
