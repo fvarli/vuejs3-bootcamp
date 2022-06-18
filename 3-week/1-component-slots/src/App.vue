c7b55236c185e70e01fe280b61260e4f593ff05b
Ferzender Varli <fvarlii@gmail.com>
2022-06-18 17:52:41 +0300

    vue-slot

---
+++ 3-week/1-component-slots/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <!--<SlotSection :onSave="onSave" :title="'Modal Title'" :content="content" />-->
+
+    <SlotSection>
+        <template #title>
+            <h1>Title with Slot</h1>
+        </template>
+        <template v-slot:content>
+            <p class="text-green">Text with Content</p>
+        </template>
+        <template #default>
+            <p>Default</p>
+        </template>
+    </SlotSection>
+
+</template>
+
+<script>
+import SlotSection from '@/components/SlotSection'
+
+export default {
+    name: 'App',
+    components: {
+        SlotSection
+    },
+
+    data() {
+        return {
+            content: `<h3 class="text-red">Modal Content</h3>`
+        }
+    },
+    methods: {
+        onSave() {
+            alert("saved");
+        }
+    }
+}
+</script>
---
+++ 3-week/1-component-slots/src/components/SlotSection.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <slot name="title" />
+        </div>
+        <hr>
+        <div class="content my-2">
+            <slot name="content" />
+        </div>
+        <slot/>
+        <hr>
+        <div class="footer text-right">
+            <button>Close</button>
+            <button class="green" @click="onSave">Save</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['content','title','onSave']
+}
+</script>
